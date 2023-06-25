2778a5f4412bdf317eca5e230666e16d7f5e17ec
ElizabethD513 <129693714+ElizabethD513@users.noreply.github.com>
2023-06-25 00:35:10 -0400

    remove node modules folder

--- index.js
+++ index.js
@@ -1,256 +1,150 @@
-let baseURL = "https://6495d01db08e17c91792b7ba.mockapi.io/api/v1/pets"
-function populateTable(data) {
-    alert(data)
-}
+class PetService {
+    static url = "https://6495d01db08e17c91792b7ba.mockapi.io/api/v1/pets"
 
-function getData() {
-    $.get(baseURL, populateTable)
-        
-
-}
-
-function createTableRow(pet) {
-    let dynamicTr = `
-    <tr>
-    <td>${pet.id}</td>
-    <td>${pet.petType}</td>
-    <td>${pet.petName}</td>
-    <td>${pet.petSex}</td>
-    <td>${pet.arrivalDate}</td>
-    <td><button class='btn btn-danger btn-sm' data-id="${pet.id}">delete</button></td>
-    <td><button class='btn btn-info btn-sm' data-id="${pet.id}">edit</button></td>
-    </tr>`;
-
-}
-getData()
-
-
-
-
-/*let emptyRow = "<tr><td colspan='5' class='text-center'> No Records Available</td></tr>";
-
-$(document).ready(function() {
-    $("#tblData tbody").append(emptyRow); //adds the empty row once page loads
-
-    $("#btnAdd").click(function() { //gets value of each form input
-        let petType = $("#txtType").val().trim();
-        let name = $("#txtName").val().trim();
-        let sex = $("#txtSex").val().trim();
-        let arrivalDate = $("#txtArrival").val().trim();
-        let mobile = $('#txtMobile').val.trim();
-
-        // console.log({petType, name, sex, arrivalDate});
-
-        //for second table: applying to adopt a pet. name/address/mobile
-        const mobileValidation = new RegExp(/^[\d]*$/gi).test(mobile) && mobile.length === 10 //checks for 10 numbers for mobile #
-        //console.log(mobile, isNumber: new RegExp(/^[\d]*$/gi).test(mobile), mobileLength: mobile.length === 10 )
-
-    });
-    if (petType === "" || name === "" || sex === "" || arrivalDate === "") {
-        alert("Please provide values"); //alert user of missing values
-        return;
-    } else if (!mobileValidation) {
-        alert("Please enter a valid phone number with 10 digits");
-        return;
-    } else {
-        if ($("#tblData tbody").children().children().length == 1) {
-            $("#tblData tbody").html("");
-
-            let formattedNumber = function(num) {
-                let beginning = mobile.slice(0, 3);
-                let middle = mobile.slice(3, 6);
-                let end = mobile.slice(6);
-
-                return `$(beginning)-$(middle)-$(end)`;
-            }
-            console.log(formattedNumber(mobile))
-
-            let srNo = $("#tblData tbody").children().length + 1; //get number of table rows to increase row id by 1
-
-            let dynamicTr = `
-        <tr>
-        <th>${srNo}</th>
-        <th>${petType}</th>
-        <th>${Name}</th>
-        <th>${Sex}</th>
-        <th>${arrivalDate}</th>
-        <td>${formattedNumber(mobile)}</td>
-        <td><button class='btn btn-danger btn-sm'>delete</button></td>
-        </tr>`;
-
-            $("#tblData tbody").append(dynamicTr); //appending dynamic string to table tbody
-
-            $("#txtpetType").val(""); //reset inputs to empty strings
-            $("#txtName").val("");
-            $("#txtSex").val("");
-            $("#txtarrivalDate").val("");
-            $("#txtMobile").val("");
-
-            $(".btn-sm").click(function() { //add delete functionality to each row
-
-                $(this).parent().parent().remove();
-                if ($("#tblData tbody").children().children().length == 0) {
-                    $("#tblData tbody").append(emptyRow);
-
-                }
-            })
-
-
-        }
-
-    }
-
-
-    //console.log()
-
-});
-
-*/
-
-
-/*class House {  //the structure for the house
-    constructor(name) {
-        this.name = name;
-        this.room = [];
-
-    }
-    addRoom(name, area) {
-        this.rooms.push(new Room(name, area));
-    }
-}
-
-class Room {
-    constructor(name, area) {
-        this.name = name;
-        this.area = area;
-    }
-}
-//the service- how you're going to send how we're going to send the htp requests
-class HouseService {
-    static url = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
-//methods to send the request:
-    static getAllHouses() { //takes no parameters because its going to take all the houses
+    //methods to send the request:
+    static getAllPets() { //takes no parameters because its going to get all the pets
         return $.get(this.url);
-    }//retrieving a specific house from the api:
-    static getHouse(id) {
-        return $.get(this.url + '/$(id)');
     }
-    static createHouse(house) { 
-        return $.post(this.url, house);
+
+    //retrieving a specific pet from the api by the id:
+    static getPet(id) {
+        return $.get(this.url + `/${id}`);  //makes a .get request for the url and concatenates the id
     }
-    static updateHouse(house) {
+
+    static addPet(pet) {        //
+        return $.post(this.url, pet);   //makes a post request to add a pet 
+    }
+
+    static updatePet(pet, petId) {  //this is a PUT request to update the pet on the server
         return $.ajax({
-        url: this.url + '/$house._id)',
-        dataType: 'json',
-        data: JSON.stringify(house),
-        contentType: 'application/json',
-        tyoe: 'PUT'
+            url: this.url + `/${petId}`,
+            dataType: 'json',
+            data: JSON.stringify(pet),  //this is converting the pet object to a JSON string
+            contentType: 'application/json',
+            type: 'PUT'
         });
     }
-   
-    static deleteHouse(id) {
+
+    static deletePet(id) {  //A delete request to delete the pet on the server by ID
         return $.ajax({
-            url: this.url + '/${id}',
+            url: this.url + `/${id}`,
             type: 'DELETE'
         });
     }
 }
 
- class DOMManager {
-    static house;
+class DOMManager {
+    static pets = []  
 
-    static getAllHouses() {
-        HouseService.getAllHouses().then(houses => this.render(houses));
-
-
+    static getAllPets() {   //gets all pets from the server and renders the table rows
+        PetService.getAllPets().then(pets => this.render(pets))
     }
 
-    static createHouse() {
-        HouseService.createHouse(new House(name))
-    }
-    static deleteHouse(id) {
-        HouseService.deleteHouse(id)
-        .then(() => {
-            return HouseService.getAllHouses();
+    static addPet(pet) {  //
+        PetService.addPet(pet).then(pet => {  //after the pet is added to the server and the post request is fulfilled, the then function is called 
+            this.getAllPets()   //getAllPets from the server and lists them in the table
+            this.resetForm()    //resets the form
         })
-        .then((houses) => this.render(houses));
     }
 
-    static addRoom(id) {
-        for(let house of this.houses) {
-            if (house._id == id){
-                house.rooms.push(new Room($('#${house._id}-room-name').val())); //jquery then template literal then id
-                 HouseService.uodateHouse(house)
-                .then(() => {
-                  return HouseService.getAllHouses();
+    static editPet(id){
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+        let petToUpdate = this.pets.find(pet => pet.id === `${id}`)   //this finds the pet in the DOMManager by ID
+        this.populateForm(petToUpdate)                              
+        $('#btnAdd').val(`Update ${petToUpdate.petName}`)    //adds the name to be edited to the 'add new pet' button
+    }
+
+    static updatePet(pet, petId){
+        PetService.updatePet(pet, petId).then(pet => {  //updates pet on the server by id and then renders all pets in the table, and resets the form
+            this.getAllPets()
+            this.resetForm()
+        })
+    }
+
+    static populateForm(pet) {
+        $('#petId').val(pet.id)  //puts the pet id into the input with the ID petId
+        $('#petType').val(pet.petType)  
+        $('#petName').val(pet.petName)
+        $('#petSex').val(pet.petSex)
+        $('#arrivalDate').val(pet.arrivalDate)
+    }
+
+    static resetForm(){
+        //https://stackoverflow.com/questions/6364289/clear-form-fields-with-jquery
+        $("input[type=text], input[type=hidden]").val("");   //takes the input text and input hidden and sets their values to an empty string
+        $('#btnAdd').val('Add New Pet')
+    }
+
+    static deletePet(id){           //deletes pet from the server by id then renders all pets in the table
+        PetService.deletePet(id).then(() => {
+            this.getAllPets()
+        })
+    }
+
+    static render(pets){    
+        this.pets = pets    //setting DOMManager.pets equal to pets from the server
+        $('#tblData tbody').empty()   //clearing the table every time we render pets 
+        for(let i=0; i<pets.length; i++){  
+            let pet = pets[i]   
+            let row = this.createTableRow(pet)
+            $('#tblData tbody').append(row)
+        }
+        // https://getbootstrap.com/docs/5.0/components/tooltips/
+        // https://stackoverflow.com/questions/33584392/bootstraps-tooltip-doesnt-disappear-after-button-click-mouseleave
+        let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))   //makes the tooltip appear to edit or delete
+        let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            let toolTip = new bootstrap.Tooltip(tooltipTriggerEl, {trigger:'hover'})
+            $(tooltipTriggerEl).click(function(e){
+                // needed because when the row is deleted the tool tip does not disappear on its own
+                $(this).tooltip('hide')
             })
-            .then((houses) => this.render(houses));
-         }
-        }
-    }
-    static deleteRoom(houseId, roomId) {
-        for (let house of this.houses) {
-            if (house._id == houseId){
-                for (let room of house.rooms){
-                    if(room._id == roomId) {
-                        house.rooms.splice(house.rooms.indexOf(room), 1);
-                        HouseService.updateHouse(house)
-                        .then(() => {
-                            return HouseService.getAllHouses();
-                        })
-                        .then((houses) => this.render(houses));
 
-                    }
-                }
+            return toolTip
+        })
+
+        // https://api.jquery.com/hover/
+        // https://fontawesome.com/icons/trash-can?f=classic&s=solid&an=beat
+        // adds animation to edit and delete button icons
+        $( ".btn-update, .btn-delete" ).hover(      //makes the pulsing animation when hovering above edit or delete buttons
+            function() {
+                $(this).find('i').addClass('fa-beat');  //when you move your mouse over the button, the class 'fa-beat' gets added to the 'i' tag
+            }, function() {
+                $(this).find('i').removeClass('fa-beat'); //when you move your mouse off the button, the class 'fa-beat' gets removed from the 'i' tag
             }
-        }
-
+        );
     }
 
-    static render(houses) {
-        this.houses = houses;
-        $('#app').empty();
-        for (let house of houses){
-            $('#app').prepend(
-             `<div id="$(house._id") class="card">
-                <div class="card-header">
-                    <h2>${house.name}</h2>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteHouse('${house._id}')">Delete</button>
-                </div>
-                <div class="card-body">
-                  <div class="card">
-                    <div class="row">
-                        <div class="col-sm">
-                         <input type="text" id="${house._id}-room-name" class="form-control" placeholder="Room Name">
-                        </div>
-                        <div class="col-sm">
-                          <input type="text" id="${house._id}-room-area" class="form-control" placeholder="Room Area">
-                        </div>
-                    </div>
-                    <button id="${house._id}-new-room" onclick="DOMManager.addRoom('${house._id}')" class="btn btn-primary form-control">Add</button>
-                </div>
-         </div>
-        </div><br>`
-            );
-    
-            for (let room of house.rooms) {
-                $('#${house._id}').find('.card-body').append(
-                `<p>
-                    <span id="name-${room._id}"><strong>Name: </strong> ${room.name}</span>
-                    <span id="name-${room._id}"><strong>Area: </strong> ${room.area}</span>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id}','${room._id}')">Delete Room</button>`<br>
+    static createTableRow(pet) {  //this creates the new table row using the new data that was entered and returns it as a string
+        let dynamicTr = `
+            <tr>
+            <td class="text-center">${pet.id}</td>
+            <td>${pet.petType}</td>
+            <td>${pet.petName}</td>
+            <td>${pet.petSex}</td>
+            <td>${pet.arrivalDate}</td>
+            <td class="text-center"><button class='btn btn-info btn-sm btn-update' data-bs-toggle="tooltip" data-bs-placement="top" title="Edit ${pet.petName}" onclick="DOMManager.editPet(${pet.id})"><i class="fa-solid fa-pen-to-square"></i></button></td>
+            <td class="text-center"><button class='btn btn-danger btn-sm btn-delete' data-bs-toggle="tooltip" data-bs-placement="top" title="Delete ${pet.petName}" onclick="DOMManager.deletePet(${pet.id})"><i class="fa-solid fa-trash-can"></i></button></td>
+            </tr>`;
+        return dynamicTr
+    }
+}
 
-                
+$('#petForm').submit(function(e){   //the form has the id petForm. This is adding an event listener for the submit event to the form.
+    e.preventDefault()              // to prevent the browser from submitting the form to the server
+    let petId = $('#petId').val()   //defining the variable petId as the value of the input with the ID petId
+    let petData = {                 //defining the variable petData as an object
+        petType:$('#petType').val(), //a property of petData which adds the value of the input with the ID petType to the petData object
+        petName:$('#petName').val(),  
+        petSex:$('#petSex').val(),
+        arrivalDate:$('#arrivalDate').val()
+    }
 
-            }
-            }
+    if(petId){          //if petId has a value, then update the pet, else add the pet
+        DOMManager.updatePet(petData, petId)
+    } else {
+        DOMManager.addPet(petData)
         
     }
- }
- 
-$('#create-new-house').click() => {
-            DOMManager.createHouse($('#new-house-name').val());
-            $('#new-house-name').val('');
-};
-DOMManager.getAllHouses();
-*/
+})
+
+DOMManager.getAllPets();
